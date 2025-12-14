5dbc32fe5c8559c3802d5b195af1c4a98604ec36
Ayush-code30 <jainaayush854@gmail.com>
2025-12-14 20:47:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,9 @@ if submit_eval or submit_match:
 # Footer
 st.markdown("---")
 st.markdown(
-    "<p style='text-align: center; font-size: 14px; color: gray;'>© 2025 All rights reserved by <strong>Tilkesh Tailor</strong></p>",
+    "<p style='text-align: center; font-size: 14px; color: gray;'>© 2025 All rights reserved by <strong>Ayush Jain</strong></p>",
     unsafe_allow_html=True
 )
+
 
 
